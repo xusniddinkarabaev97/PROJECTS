b17f16994883a6ee181c65c7f4e08039a7f4eb22
xusniddinkarabaev97 <xusniddinkarabaev2@gmail.com>
2026-07-13 17:15:47 +0500

    Re-add 8 dispensers for station 1283 with ID display

--- gzs_billing/seed_1283.sql
+++ gzs_billing/seed_1283.sql
@@ -1,9 +1,12 @@
-INSERT INTO filling_stations ("Id", "Name", "Address", "Region", "CreatedAt") VALUES (1283, 'GZS 1283', 'UGaz', 'Tashkent', NOW()) ON CONFLICT DO NOTHING;
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 1', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 2', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 3', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 4', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 5', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 6', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 7', 'Метан', NOW());
-INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES (1283, 'Колонка 8', 'Метан', NOW());
+DELETE FROM dispensers WHERE "FillingStationId" = 1283;
+INSERT INTO dispensers ("FillingStationId", "Name", "FuelType", "CreatedAt") VALUES
+(1283, 'Колонка 1', 'Метан', NOW()),
+(1283, 'Колонка 2', 'Метан', NOW()),
+(1283, 'Колонка 3', 'Метан', NOW()),
+(1283, 'Колонка 4', 'Метан', NOW()),
+(1283, 'Колонка 5', 'Метан', NOW()),
+(1283, 'Колонка 6', 'Метан', NOW()),
+(1283, 'Колонка 7', 'Метан', NOW()),
+(1283, 'Колонка 8', 'Метан', NOW());
+SELECT 'Done! IDs:' AS result;
+SELECT "Id", "Name" FROM dispensers WHERE "FillingStationId" = 1283 ORDER BY "Id";
